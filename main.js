5c3a3602de2d633465a8a20af767509ac17b07f2
Bob Naydenov <bob.naydenov@hotmail.com>
2018-03-11 18:52:12 +0000

    Setup project structure

---
+++ main.js
@@ -0,0 +1,41 @@
+const electron = require('electron')
+const {app, BrowserWindow, ipcMain} = require('electron')
+const { blockWindowAds, adBlocker } = require('electron-ad-blocker')
+const path = require('path')
+const url = require('url')
+const fs = require('fs')
+
+let messengerWindow = null;
+
+app.on('ready', function() {
+
+  const {width, height} = electron.screen.getPrimaryDisplay().workAreaSize
+
+    messengerWindow = new BrowserWindow({
+        height: height * 0.50,
+        width: width * 0.45,
+        frame: false,
+        transparent: false,
+        icon: path.join(__dirname, 'app/build/icon.png'),
+        alwaysOnTop: true
+    })
+
+    messengerWindow.loadURL('file://' + __dirname + '/app/index.html');
+
+    const options = {
+      verbose: false,
+      logger: console,
+    }
+
+    blockWindowAds(messengerWindow, options);
+
+
+})
+
+ipcMain.on('button-press-hide', (event, arg) => {
+  messengerWindow.minimize()
+})
+
+ipcMain.on('button-press-close', (event, arg) => {
+  app.quit()
+})
