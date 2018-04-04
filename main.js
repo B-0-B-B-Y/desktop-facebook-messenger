c528157504d139343933a703a546f3ab0bd2d9a4
Bob Naydenov <bob.naydenov@hotmail.com>
2018-04-04 20:25:15 +0100

    Make updating the app automatic

--- main.js
+++ main.js
@@ -9,6 +9,8 @@ let messengerWindow = null;
 
 app.on('ready', function() {
 
+  autoUpdater.checkForUpdatesAndNotify()
+
   const {width, height} = electron.screen.getPrimaryDisplay().size
 
     messengerWindow = new BrowserWindow({
@@ -39,34 +41,7 @@ app.on('ready', function() {
 
     messengerWindow.loadURL('file://' + __dirname + '/app/index.html');
 
-    autoUpdater.checkForUpdates()
-
 })
-
-function sendStatusToWindow(text) {
-  messengerWindow.webContents.send('message', text);
-}
-autoUpdater.on('checking-for-update', () => {
-  sendStatusToWindow('Checking for update...');
-})
-autoUpdater.on('update-available', (info) => {
-  sendStatusToWindow('Update available.');
-})
-autoUpdater.on('update-not-available', (info) => {
-  sendStatusToWindow('Update not available.');
-})
-autoUpdater.on('error', (err) => {
-  sendStatusToWindow('Error in auto-updater. ' + err);
-})
-autoUpdater.on('download-progress', (progressObj) => {
-  let log_message = "Download speed: " + progressObj.bytesPerSecond;
-  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
-  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
-  sendStatusToWindow(log_message);
-})
-autoUpdater.on('update-downloaded', (info) => {
-  sendStatusToWindow('Update downloaded');
-});
 
 ipcMain.on('button-press-hide', (event, arg) => {
   messengerWindow.minimize()
