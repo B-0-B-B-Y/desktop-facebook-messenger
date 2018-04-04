4c6b9294ab1c2e3ff71c24632ae851795e43ba9e
Bob Naydenov <bob.naydenov@hotmail.com>
2018-04-04 03:38:23 +0100

    Reconfigure update settings

--- main.js
+++ main.js
@@ -43,15 +43,30 @@ app.on('ready', function() {
 
 })
 
-// when the update has been downloaded and is ready to be installed, notify the BrowserWindow
-autoUpdater.on('update-downloaded', (info) => {
-    win.webContents.send('updateReady')
-});
-
-// when receiving a quitAndInstall signal, quit and install the new version
-ipcMain.on("quitAndInstall", (event, arg) => {
-    autoUpdater.quitAndInstall();
+function sendStatusToWindow(text) {
+  messengerWindow.webContents.send('message', text);
+}
+autoUpdater.on('checking-for-update', () => {
+  sendStatusToWindow('Checking for update...');
 })
+autoUpdater.on('update-available', (info) => {
+  sendStatusToWindow('Update available.');
+})
+autoUpdater.on('update-not-available', (info) => {
+  sendStatusToWindow('Update not available.');
+})
+autoUpdater.on('error', (err) => {
+  sendStatusToWindow('Error in auto-updater. ' + err);
+})
+autoUpdater.on('download-progress', (progressObj) => {
+  let log_message = "Download speed: " + progressObj.bytesPerSecond;
+  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+  sendStatusToWindow(log_message);
+})
+autoUpdater.on('update-downloaded', (info) => {
+  sendStatusToWindow('Update downloaded');
+});
 
 ipcMain.on('button-press-hide', (event, arg) => {
   messengerWindow.minimize()
